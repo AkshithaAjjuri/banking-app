cfb2aff7dbff6b5fde1217f34ba464cdd1aeeb08
Aksh <aksh87@gmail.com>
2025-09-10 00:18:56 -0700

    mearge dev into feature/rest-accounts-backend

---
+++ backend/src/models/Account.js
@@ -0,0 +1,58 @@
+import mongoose from "mongoose";
+// the Transaction schema is an embedded document 
+const transactonSchema= new mongoose.Schema({
+    txnId:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    type:{
+        type:String,
+        enum:["deposit","withdraw","transfer"],
+        required:true
+    },
+    amount:{
+        type:Number,
+        required:true,
+    },
+    timestamp:{
+        type:Date,
+        default:Date.now
+    },
+    status:{
+        type:String,
+        enum:["success","failed","pending"],
+        dafault:"success"
+    },
+
+})
+//The main Account schema 
+
+const accountSchema =new mongoose.Schema({
+    accountNumber:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    type:{
+        type:String,
+        enum:['savings',"current"],
+        required:true
+    },
+    status:{
+        type:String,
+        enum:["active","frozen","closed"],
+        default:"active",
+
+    },
+    transactions : [transactonSchema],//the embedded array of transaction 
+
+});
+export const Account=mongoose.model("Account",accountSchema)
+export const Transaction =mongoose.model("Transaction",transactonSchema)
+export default{Account,Transaction}
